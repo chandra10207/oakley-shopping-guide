e96c804b4d3dbb0cc2a1d7f754c0e3b7ba074b06
chandra <chandra8848@outlook.com>
2014-12-17 15:37:24 +0545

    Detail page created. js implemented

---
+++ js/language.js
@@ -0,0 +1,9 @@
+function changeFlag()
+{
+	var img=document.getElementById("flagImage");
+	var currentCountry=document.getElementById("flag");
+	img.setAttribute("src",currentCountry[currentCountry.selectedIndex].getAttribute("src"));
+	img.setAttribute("alt",currentCountry[currentCountry.selectedIndex].getAttribute("alt"));
+}
+
+//document.getElementById("ll").add('active');
